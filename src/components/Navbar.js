4ac9342821cfaf1016de601f38565ca21412fd0b
pki071120 <ddametyy@gmail.com>
2025-07-14 18:26:47 +0900

    Apply React-Scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaTimes, FaAlignJustify } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
@@ -10,11 +11,31 @@ const Navbar = () => {
 				<div className="flex items-center">
 					<h1 className="mr-4 text-3xl font-bold sm:text-4xl ">Chatty</h1>
 					<ul className="hidden md:flex">
-						<li>Home</li>
-						<li>About</li>
-						<li>Support</li>
-						<li>Feature</li>
-						<li>Feedback</li>
+						<li>
+							<Link to="home" smooth={true} duration={500}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to="about" smooth={true} duration={500}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to="support" smooth={true} duration={500}>
+								Support
+							</Link>
+						</li>
+						<li>
+							<Link to="feature" smooth={true} duration={500}>
+								Feature
+							</Link>
+						</li>
+						<li>
+							<Link to="testimonials" smooth={true} duration={500}>
+								Feedback
+							</Link>
+						</li>
 					</ul>
 				</div>
 				<div className="hidden md:flex pr-4">
@@ -28,12 +49,56 @@ const Navbar = () => {
 				</div>
 			</div>
 			<ul className={nav ? "absolute bg-zinc-200 w-full px-8" : "hidden"}>
-				<li className="border-b-2 border-zinc-300 w-full">Home</li>
-				<li className="border-b-2 border-zinc-300 w-full">about</li>
-				<li className="border-b-2 border-zinc-300 w-full">Support</li>
-				<li className="border-b-2 border-zinc-300 w-full">Feature</li>
-				<li className="border-b-2 border-zinc-300 w-full">Feedback</li>
-
+				<li className="border-b-2 border-zinc-300 w-full">
+					<Link
+						onClick={() => setNav(false)}
+						to="home"
+						smooth={true}
+						duration={500}
+					>
+						Home
+					</Link>
+				</li>
+				<li className="border-b-2 border-zinc-300 w-full">
+					<Link
+						onClick={() => setNav(false)}
+						to="about"
+						smooth={true}
+						duration={500}
+					>
+						About
+					</Link>
+				</li>
+				<li className="border-b-2 border-zinc-300 w-full">
+					<Link
+						onClick={() => setNav(false)}
+						to="support"
+						smooth={true}
+						duration={500}
+					>
+						Support
+					</Link>
+				</li>
+				<li className="border-b-2 border-zinc-300 w-full">
+					<Link
+						onClick={() => setNav(false)}
+						to="feature"
+						smooth={true}
+						duration={500}
+					>
+						Feature
+					</Link>
+				</li>
+				<li className="border-b-2 border-zinc-300 w-full">
+					<Link
+						onClick={() => setNav(false)}
+						to="testimonials"
+						smooth={true}
+						duration={500}
+					>
+						Feedback
+					</Link>
+				</li>
 				<div className="flex flex-col my-4">
 					<button className="px-8 py-3 mb-4 text-slate-600 bg-transparent">
 						Sign In
